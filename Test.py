1714260529b780c77f5975f2b58ef68a3f2026f7
SDim44 <68907465+SDim44@users.noreply.github.com>
2023-06-05 18:51:21 +0200

    Update

---
+++ Test.py
@@ -0,0 +1,9 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+print(tf.__version__)
+
+
+DATA_PATH = '/Users/hafsa/Documents/datasets/ukdale2.h5'
